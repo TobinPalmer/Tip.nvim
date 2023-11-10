956d7a460d2134be46f45a5d1f752b760b273fcd
TobinPalmer <tobinkpalmer@gmail.com>
2023-11-10 10:31:15 -0800

    Added gif to demo

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 <div align="center">
   <p><strong>A simple plugin to greet you with a tip when you launch Neovim</strong></p>
 </div>
+
+![Demo](./static/demo.gif)
+
+## Requirements
+- [nvim-notify](https://github.com/rcarriga/nvim-notify)
+- [plenary.nvim](https://github.com/nvim-lua/plenary.nvim)
+
+## Installation
+
+```lua
+-- Lazy.nvim
+return {
+  "TobinPalmer/Tip.nvim",
+  event = "VimEnter",
+  init = function()
+    -- Default config
+    --- @type Tip.config
+    require("tip").setup({
+      title = "Tip!",
+      url = "https://vtip.43z.one",
+    })
+  end,
+}
+```
--- lua/tip.lua
+++ lua/tip.lua
@@ -15,27 +15,26 @@ M.config = {
 ---@param params Tip.config
 M.setup = function(params)
   M.config = vim.tbl_deep_extend('force', {}, M.config, params or {})
+  vim.api.nvim_create_autocmd('VimEnter', {
+    callback = function()
+      local job = require 'plenary.job'
+
+      job
+        :new({
+          command = 'curl',
+          args = { M.config.url },
+          on_exit = function(j, exit_code)
+            local res = table.concat(j:result())
+            if exit_code ~= 0 then
+              res = 'Error fetching tip: ' .. res
+            end
+
+            vim.notify(res, 2, { title = M.config.title })
+          end,
+        })
+        :start()
+    end,
+  })
 end
-
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    local job = require 'plenary.job'
-
-    job
-      :new({
-        command = 'curl',
-        args = { M.config.url },
-        on_exit = function(j, exit_code)
-          local res = table.concat(j:result())
-          if exit_code ~= 0 then
-            res = 'Error fetching tip: ' .. res
-          end
-
-          vim.notify(res, 2, { title = M.config.title })
-        end,
-      })
-      :start()
-  end,
-})
 
 return M
